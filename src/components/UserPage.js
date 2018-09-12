2cbdfbe5abfa851b69145e03c8cf2d766971325c
Mark Hale <mark.hale94@gmail.com>
2018-09-12 11:48:51 -0500

    added cards for each game as well as a button that queries for the individual game data. although it only posts the info to the console at the moment.

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DataManager from './modules/DataManager'
+import GameCardDisplay from './games/GameCardDisplay'
 
 export default class UserPage extends Component {
   state = {
@@ -16,24 +17,12 @@ export default class UserPage extends Component {
     const newGamesArray=[]
     DataManager.search(this.state.userSearch).then((r)=>{
       for(let i=0;i<r.results.length;i++){
-        console.log(r.results[i].api_detail_url)
-        newGamesArray.push(r.results[i].api_detail_url)
-        console.log(newGamesArray)
+        newGamesArray.push(r.results[i])
         this.setState({gamesList : newGamesArray})
       }
     })
   }
-  searchForSpecificGame = ()=>{
-    DataManager.specificGameSearch(this.state.gamesList[0]).then((r)=>{
-        console.log(r.results)
-        console.log(r.results.name)
-        for(let i=0;i<r.results.genres.length;i++){
-          console.log(r.results.genres[i].name)
-        }
-        console.log(r.results.platforms)
-        console.log(r.results.description)
-      })
-  }
+
 
     render() {
       return (
@@ -45,8 +34,9 @@ export default class UserPage extends Component {
           <div className="search-input">
             <input type="text" placeholder="Search" id="userSearch" defaultValue={this.state.userSearch} onChange={this.handleFieldChange}/>
             <button onClick={this.searchForGames}>Search</button>
-            <button onClick={this.searchForSpecificGame}>Let's Try This Shit Out</button>
           </div>
+          {this.state.gamesList.map(game =>
+        <GameCardDisplay game={game} key={game.id}/>)}
         </div>
       );
     }
--- src/components/games/GameCardDisplay.js
+++ src/components/games/GameCardDisplay.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Card, Image } from 'semantic-ui-react'
+import DataManager from '../modules/DataManager'
 export default class GameCardDisplay extends Component {
-    return(){
-        <div>
-            <div>game name:</div>
-            <div>game description:</div>
-            <div>game genre:</div>
-        </div>
+    searchForSpecificGame = ()=>{
+        DataManager.specificGameSearch(this.props.game.api_detail_url).then((r)=>{
+            console.log(r.results.name)
+            for(let i=0;i<r.results.genres.length;i++){
+              console.log(r.results.genres[i].name)
+            }
+            console.log(r.results.platforms)
+          })
+      }
+    render(){
+        return(
+            <Card>
+                <Image src={(this.props.game.image.icon_url)} alt={this.props.game.name}></Image>
+                <Card.Content>
+                <Card.Header>
+                    <h2>game name:{this.props.game.name}</h2>
+                </Card.Header>
+                <Card.Description>
+                    <p>game description:{this.props.game.deck}</p>
+                </Card.Description>
+                <button onClick={this.searchForSpecificGame}>Let's Try This Shit Out</button>
+                </Card.Content>
+            </Card>
+        )
     }
 }
