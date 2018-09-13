00145a2f85a681ddf535e923a82e8000c32da57b
Mark Hale <mark.hale94@gmail.com>
2018-09-13 12:09:05 -0500

    made each modal only hit the database when the user clicks the button. it then saves the extra data to the modal specific state.

--- src/components/games/GameCardDisplay.js
+++ src/components/games/GameCardDisplay.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
 import { Card, Image,} from 'semantic-ui-react'
-import DataManager from '../modules/DataManager'
+// import DataManager from '../modules/DataManager'
 import SpecificGameModal from './SpecificGameModal'
 export default class GameCardDisplay extends Component {
-    searchForSpecificGame = ()=>{
-        let newGame={}
-    DataManager.specificGameSearch(this.props.game.api_detail_url).then((r)=>{
-        newGame.name=r.results.name
-        for(let i=0;i<r.results.genres.length;i++){
-          newGame.genres=(r.results.genres[i].name)
-        }
-        for(let i=0;i<r.results.platforms.length;i++){
-        newGame.platforms=(r.results.platforms[i].name)
-        }
-      })
-  }
     render(){
         return(
             <Card>
@@ -24,7 +12,7 @@ export default class GameCardDisplay extends Component {
                 <Card.Description>
                     game description: {this.props.game.deck}
                 </Card.Description>
-                <SpecificGameModal newGame={this.newGame} searchForSpecificGame={this.searchForSpecificGame} game={this.props.game}/>
+                <SpecificGameModal game={this.props.game}/>
                 </Card.Content>
             </Card>
         )
--- src/components/games/SpecificGameModal.js
+++ src/components/games/SpecificGameModal.js
@@ -1,33 +1,60 @@
 
 import { Button, Header, Image, Modal } from 'semantic-ui-react';
 import React, { Component } from 'react';
-
+import DataManager from '../modules/DataManager'
 
 export default class SpecificGameModal extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      title: undefined,
-      description: undefined,
-      completed: false,
+      image:null,
+      title: null,
+      description: null,
+      genre: null,
+      platform: null
     }
   }
-  
+
+searchForSpecificGame = ()=>{
+DataManager.specificGameSearch(this.props.game.api_detail_url)
+  .then((game) => {
+    let newState={}
+    newState.image=game.results.image.medium_url
+    newState.title=game.results.name
+    newState.description=game.results.deck
+    for(let i=0;i<game.results.genres.length;i++){
+          if(i===0){
+            newState.genre=(game.results.genres[i].name)
+          }else if (i!==0){
+          newState.genre+=", "+(game.results.genres[i].name)
+          }
+        }
+    for(let j=0;j<game.results.platforms.length;j++){
+      if(j===0){
+        newState.platform=(game.results.platforms[j].name)
+      }else if (j!==0){
+          newState.platform+=", "+(game.results.platforms[j].name)
+      }
+        }
+      this.setState(newState)
+  })
+}
+
 render(){
     return(
-          <Modal trigger={<Button onClick={this.props.searchForSpecificGame}>Let's Do This Shit</Button>}>
+          <Modal trigger={<Button onClick={this.searchForSpecificGame}>Let's Do This Shit</Button>}>
     <Modal.Content image>
       <Image
         wrapped
         size="medium"
-        src={(this.props.game.image.medium_url)}
+        src={(this.state.image)}
       />
       <Modal.Description>
-        <Header>{this.props.game.name}</Header>
+        <Header>{this.state.title}</Header>
         <p>
-          {this.props.game.deck}
+          {this.state.description}
         </p>
-        <Button onClick={console.log(this.props.newGame)}>Add {this.props.game.name} to Your Collection?</Button>
+        <Button>Add {this.state.title} to Your Collection?</Button>
       </Modal.Description>
     </Modal.Content>
   </Modal>
