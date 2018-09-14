7770b25244d4ab6758cb447ab23f77b1f52ed98e
Mark Hale <mark.hale94@gmail.com>
2018-09-14 15:57:18 -0500

    added button for game randomizer, have not added click event yet

--- src/components/games/GameCollectionList.js
+++ src/components/games/GameCollectionList.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import GameCollectionCardDisplay from './GameCollectionCardDisplay'
-import { Card } from 'semantic-ui-react'
+import { Card, Button} from 'semantic-ui-react'
 
 export default class GameCollectionList extends Component {   
+
     render(){
         return (
-            <Card.Group>
-                {this.props.game.map(game =>
-            <GameCollectionCardDisplay editGame={this.props.editGame} deleteGame={this.props.deleteGame} game={game} key={game.id}/>)}
-            </Card.Group>
+        <div>
+                <Button fluid={true} color="youtube" size="massive" >Pick a Game for Me</Button>
+                <Card.Group>
+                    {this.props.game.map(game =>
+                <GameCollectionCardDisplay editGame={this.props.editGame} deleteGame={this.props.deleteGame} game={game} key={game.id}/>)}
+                </Card.Group>
+          
+        </div>
         )
     }
 }
