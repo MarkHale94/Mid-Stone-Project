45d57f8b8933a5153b0e446402f124432d1fb413
Mark Hale <mark.hale94@gmail.com>
2018-10-01 14:38:49 -0500

    forgot to save a comment made in the last commit

--- src/components/nav/Logout.js
+++ src/components/nav/Logout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 export default class Logout extends Component {
+    //simple function that takes the info in session storage, clears out that info and then forces a page reload to bring the user back to the login page.
     logoutFunction = ()=>
     {
         sessionStorage.clear();
